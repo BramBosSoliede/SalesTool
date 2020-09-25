93a35ab1443b67081f3f5f77a631ef63cdfe18cc
BramBosSoliede <bram@soliede.nl>
2020-09-25 15:32:01 +0200

    Initial commit

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(1)
+            ->visit('/dashboard');
+            $browser->assertSee("Jetstream");
+        });
+    }
+}
